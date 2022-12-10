c661d5ab791a355ca6c49a21644e8d596922306d
Guilherme Malta <guilhermeomalta0@gmail.com>
2022-12-10 20:20:24 -0300

    Creating Header component

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import { Flex } from "@chakra-ui/react";
+import Image from "next/image";
+import logo from "../assets/logo.png";
+
+export const Header = () => {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      h="20"
+      mx="auto"
+      px="6"
+      borderBottom="1px solid"
+      borderColor="gray.200"
+      align="center"
+    >
+      <Image src={logo} alt="logo" style={{ margin: "0 auto" }} />
+    </Flex>
+  );
+};
